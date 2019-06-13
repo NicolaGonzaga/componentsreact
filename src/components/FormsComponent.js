17c013aa50962a6f156b1e136e94b86e9d5c6e22
jexperts <nicola.gonzaga@jexperts.com.br>
2019-06-13 15:11:05 -0300

    Comitar

--- src/components/FormsComponent.js
+++ src/components/FormsComponent.js
@@ -1,34 +1,45 @@
 import React, { Component } from 'react';
-import { __asyncValues } from 'tslib';
+import PropTypes from 'prop-types';
+
 
 class FormsComponent extends Component {
 
-    handleInputChange (event) {
-        const email = target.name;
-        const password = targer.name;
+    state = {
+        email: null,
+        password: null
+    };
+    
+    onChangeEmail = (event) => {
+        this.setState({email: event.target.value});
+    };
 
-        this.state({
-            [email]:email
-        });
+    onChangePassword = (event) => {
+        this.setState({password: event.target.value});
+    };
+
+    handleClick = () => {
+        alert("Clique aqui");
     }
+    
 
     render(){
         return (
         <form>
         <div class="form-group">
           <label for="exampleInputEmail1">E-mail</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" value=""onChange={this.handleInputChange}/>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" onChange={this.onChangeEmail} value={this.state.email}/>
           <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
         <div class="form-group">
           <label for="exampleInputPassword1">Senha</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha" value="" />
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha" onChange={this.onChangePassword}  value={this.state.password}/>
         </div>
         <div class="form-check">
           <input type="checkbox" class="form-check-input" id="exampleCheck1" />
           <label class="form-check-label" for="exampleCheck1">Check me out</label>
         </div>
-        <button type="submit" class="btn btn-primary">Vai</button>
+        <br></br>
+        <button type="submit" class="btn btn-primary">Submeter</button>
         </form>
       );
     }
